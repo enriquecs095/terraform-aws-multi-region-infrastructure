6d905770dafb9149fab13acce616188d7c43770d
Enrique Cruz <enrique.cruz@acklenavenue.com>
2022-05-17 16:40:13 -0600

    changes

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
   backend "s3" {
     region  = "us-east-1"
     #profile = "default"
-    key     = "env2/terraformstatefile"
+    key     = "master/terraformstatefile"
     bucket  = "terraformstatebucket01234"
   }
 }
