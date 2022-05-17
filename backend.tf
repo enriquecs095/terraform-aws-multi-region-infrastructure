750f13a96c09e47dc59f5dd97ad20ba4857e56d1
Enrique Cruz <enrique.cruz@acklenavenue.com>
2022-05-17 15:52:49 -0600

    change

--- backend.tf
+++ backend.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.12.0"
   backend "s3" {
     region  = "us-east-1"
-    key     = "master/terraformstatefile"
+    #profile = "default"
+    key     = "env2/terraformstatefile"
     bucket  = "terraformstatebucket01234"
   }
 }
-
