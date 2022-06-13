70ce976f93597d44224ed6c9897094907b6d3dec
Enrique Cruz <enrique.cruz@acklenavenue.com>
2022-06-13 13:48:43 -0600

    changes

--- instances.tf
+++ instances.tf
@@ -34,13 +34,6 @@ resource "aws_instance" "jenkins-master" {
   vpc_security_group_ids      = [aws_security_group.jenkins-sg.id]
   subnet_id                   = aws_subnet.subnet_1.id
 
-  provisioner "local-exec" {
-    command = <<EOF
-aws ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
- && ansible-playbook -i ansible_templates/inventory_aws/tf_aws_ec2.yml --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
-
-EOF
-  }
   tags = {
     Name = join("_", ["jenkins_master_tf", var.environment])
   }
@@ -62,12 +55,6 @@ resource "aws_instance" "jenkins-worker-oregon" {
     Name = join("_", ["jenkins_worker_tf", var.environment, count.index + 1])
   }
 
-  provisioner "local-exec" {
-    command = <<EOF
-aws ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id} \
-&& ansible-playbook -i ansible_templates/inventory_aws/tf_aws_ec2.yml --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master_ip=${aws_instance.jenkins-master.private_ip}' ansible_templates/jenkins-worker-sample.yml
-EOF
-  }
 
   depends_on = [aws_main_route_table_association.set-worker-default-rt-assoc, aws_instance.jenkins-master]
 
