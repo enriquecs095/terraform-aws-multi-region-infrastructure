205597bcbed2d252982f996e7b1e5ddefdf9fcdc
Enrique Cruz <enrique.cruz@acklenavenue.com>
2022-06-13 16:05:57 -0600

    changes

--- instances.tf
+++ instances.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "jenkins-master" {
   provisioner "local-exec" {
     command = <<EOF
 aws ec2 wait instance-status-ok --region ${var.region-master} --instance-ids ${self.id} \
- && ansible-playbook -i ansible_templates/inventory_aws/tf_aws_ec2.yml --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
+ && ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name}' ansible_templates/jenkins-master-sample.yml
 
 EOF
   }
@@ -65,7 +65,7 @@ resource "aws_instance" "jenkins-worker-oregon" {
   provisioner "local-exec" {
     command = <<EOF
 aws ec2 wait instance-status-ok --region ${var.region-worker} --instance-ids ${self.id} \
-&& ansible-playbook -i ansible_templates/inventory_aws/tf_aws_ec2.yml --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master_ip=${aws_instance.jenkins-master.private_ip}' ansible_templates/jenkins-worker-sample.yml
+&& ansible-playbook --extra-vars 'passed_in_hosts=tag_Name_${self.tags.Name} master_ip=${aws_instance.jenkins-master.private_ip}' ansible_templates/jenkins-worker-sample.yml
 EOF
   }
 
